8a2838035b335fa2c9dcacd2bfece60bee38c0a4
badass1901 <shashwatsagar19@gmail.com>
2026-05-22 21:44:59 +0530

    router provider setup, and other changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { HomePage, Registration, LoginPage } from './pages';
+import {RouterProvider } from 'react-router-dom'
+import { MyRouter } from './routes/router';
 
 const App = () => {
   return (
     <div>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/registration' element={<Registration />} />
-        <Route path='/login' element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
+      <RouterProvider router={MyRouter()} />
     </div>
   )
 }
 
 export default App
+
+
+
+{/* <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/registration' element={<Registration />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='*' element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </BrowserRouter> */}
